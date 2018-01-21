4f2582ae304639141a2102ec6ec673e46575004d
yongganzhang <1285963544@qq.com>
2018-01-21 11:03:27 +0800

    restfull风格

---
+++ src/main/java/com/shsxt/wc/controller/RestController.java
@@ -0,0 +1,41 @@
+package com.shsxt.wc.controller;
+
+import com.shsxt.wc.entity.User;
+import com.shsxt.wc.service.UserService;
+import com.shsxt.wc.util.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+//@Controller
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/user/{id}")
+//    @ResponseBody
+    public Result queryUserById(@PathVariable("id") int id) {
+        return userService.queryUserById(id);
+    }
+
+    @PostMapping("/user")
+//    @ResponseBody
+    public Result insertUserById(User user) {
+        return userService.insertUser(user);
+    }
+
+    @DeleteMapping("/user/{id}")
+//    @ResponseBody
+    public Result delUserById(@PathVariable int id) {
+        return userService.delUser(id);
+    }
+
+    @PutMapping("/user")
+//    @ResponseBody
+    @RequestMapping(value = "/user",method = RequestMethod.PUT)
+    public Result updateUserById(User user) {
+        return userService.updateUser(user);
+    }
+
+}
--- src/main/java/com/shsxt/wc/service/UserService.java
+++ src/main/java/com/shsxt/wc/service/UserService.java
@@ -5,7 +5,9 @@ import com.shsxt.wc.entity.User;
 import com.shsxt.wc.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,16 +24,45 @@ public class UserService {
     @Autowired(required = false)
     private HttpSession session;
 
-    public Result userLogin (String userName, String userPwd) {
+    public Result userLogin(String userName, String userPwd) {
         Map<String, Object> params = new HashMap<String, Object>();
-        params.put("userName",userName );
-        params.put("userPwd",userPwd );
+        params.put("userName", userName);
+        params.put("userPwd", userPwd);
         User one = userDao.findOne(params);
         if (one == null) {
-          return  Result.fail("登陆失败");
+            return Result.fail("登陆失败");
         }
-        session.setAttribute("user",one );
-        return  Result.success("登陆成功");
+        session.setAttribute("user", one);
+        return Result.success("登陆成功");
+    }
+
+    public Result queryUserById(int id) {
+        User user = userDao.get(id);
+        return Result.success("查询成功", user);
+    }
+
+    public Result insertUser(User user) {
+        long ste = userDao.saveSte(user);
+        if (ste == 1) {
+            return Result.success("插入数据成功");
+        }
+        return Result.fail("插入数据失败");
+    }
+
+    public Result delUser(int id) {
+        long ste = userDao.delete(id);
+        if (ste == 1) {
+            return Result.success("删除数据成功");
+        }
+        return Result.fail("删除数据失败");
+    }
+
+    public Result updateUser(User user) {
+        long ste = userDao.updateSte(user);
+        if (ste == 1) {
+            return Result.success("更新数据成功");
+        }
+        return Result.fail("更新数据失败");
     }
 
 }
